4f9d9fe614892fb0d740c4fc9013fc85d1e7b3e4
1devhall <devoughnh@gmail.com>
2020-01-22 15:22:43 -0500

    done

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,10 +4,14 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
+  console.log('the props', props);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      {props.parentData.map(p => (
+        <Post key = {p.imgUrl} dataFromParent = {p} />
+      )
+        )/* map through data here */}
     </div>
   );
 };
